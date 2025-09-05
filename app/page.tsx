8fe580f43253b9adddc1a973a017a67454977617
Nicoj3000 <nicoj3000its@gmail.com>
2025-09-05 12:03:40 -0500

    try3

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
       <TransitionPage />
       <div className="flex min-h-[120vh] h-full bg-no-repeat bg-gradient-cover">
         <CoverParticles />
-        
         <Introduction />
       </div>
       </I18nextProvider>
